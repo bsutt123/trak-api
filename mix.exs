8f8e694c70e8b6f4af4e8a458332d6c49301a6fc
Brady Sutton <bsutt123@gmail.com>
2018-06-30 07:01:26 -0400

    add fake elixir data options

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule Trak.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:faker_elixir_octopus, "~> 1.0.0", only: [:dev, :test]}
     ]
   end
 
